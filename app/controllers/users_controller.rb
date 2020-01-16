239b6cb88467f47ac401a7734ba9d0ab3d098258
Naya Willis <greedybrain18@gmail.com>
2020-01-16 13:32:48 -0500

    made some changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,43 @@
 class UsersController < ApplicationController
     
-    
+    get '/users/home' do 
+    @user = User.find_by(username: session[:username])
+    erb :'/users/home.html'
+  end
+  
+  get '/signup' do
+      erb :'/users/new.html'
+  end
+
+  post '/signup' do 
+      @user = User.new(params[:user])
+      if @user.save 
+          session[:username] = @user.username
+          redirect '/users/home'
+      else
+          erb :'/users/new.html'
+      end
+  end
+
+
+  get '/login' do 
+      erb :'/users/login.html'
+  end
+
+  post '/login' do 
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+          session[:username] = @user.username
+          # binding.pry
+          redirect '/users/home'
+      else
+          erb :'/users/login.html'
+      end
+  end
+
+  get '/logout' do 
+      session.clear
+      redirect '/login'
+  end
 
 end 
