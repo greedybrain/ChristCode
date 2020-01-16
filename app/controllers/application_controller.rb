a3526c25c4246355aa094624ec1aae037303b7f7
Naya Willis <greedybrain18@gmail.com>
2020-01-16 13:29:02 -0500

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,39 +9,56 @@ class ApplicationController < Sinatra::Base
     set :sessions_secret, 'john_3_16'
   end
 
-  get "/" do
-    erb :'index.html'
+  get '/' do 
+    erb :'/index.html'
+  end
+
+  get '/users/home' do 
+    @user = User.find_by(username: session[:username])
+    erb :'/users/home.html'
+  end
+  
+  get '/signup' do
+      erb :'/users/new.html'
+  end
+
+  post '/signup' do 
+      @user = User.new(params[:user])
+      if @user.save 
+          session[:username] = @user.username
+          redirect '/users/home'
+      else
+          erb :'/users/new.html'
+      end
+  end
+
+
+  get '/login' do 
+      erb :'/users/login.html'
+  end
+
+  post '/login' do 
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+          session[:username] = @user.username
+          # binding.pry
+          redirect '/users/home'
+      else
+          erb :'/users/login.html'
+      end
+  end
+
+  get '/logout' do 
+      session.clear
+      redirect '/login'
   end
 
   helpers do 
-
-    def current_user
-      User.find_by(username: session[:username])
-    end
-
+  
     def logged_in?
-      !!session[:username]
-    end 
-
-    # def user_signup
-     
-    # end
-
-    # def signup_form_or_user_home
-      
-    # end
-
-    # def login_form_or_user_home
-     
-    # end
-
-    # def user_login
-      
-    # end
-
-    # def user_logout
-    # end
-
-  end 
+      !!session[]
+    end
+  
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
     validates_uniqueness_of :username, :email
     validates_presence_of :name, :username, :email, :password
-    validates_length_of :password, :minimum => 8
+    validates :password, length: { :minimum => 8 }
 
     has_many :verses, through: :favorites 
     has_many :favorites
